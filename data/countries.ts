4a3311a10e777113e4ff35915e3e60420faf250c
Neosh <neosh11@icloud.com>
2021-04-27 10:30:06 +0800

    Update with phone numbers.

--- data/countries.ts
+++ data/countries.ts
@@ -3,6 +3,7 @@ export enum Country {
   GLOBAL,
   AUSTRALIA,
   CANADA,
+  NZ,
   USA,
   UK
 }
@@ -11,6 +12,7 @@ export const CountryMap = new Map<number, string>([
   [Country.GLOBAL, 'Global'],
   [Country.AUSTRALIA, 'Australia'],
   [Country.CANADA, 'Canada'],
+  [Country.NZ, 'New Zealand'],
   [Country.UK, 'United Kingdom'],
   [Country.USA, 'United States of America']
 ])
